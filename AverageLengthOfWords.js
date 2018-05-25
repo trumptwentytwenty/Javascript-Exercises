e74045540f56b9c6fc4acc8d1f30887022a91ef4
trumptwentytwenty <39625985+trumptwentytwenty@users.noreply.github.com>
2018-05-25 09:17:42 -0500

    Create AverageLengthOfWords.js

---
+++ AverageLengthOfWords.js
@@ -0,0 +1,10 @@
+function computeAverageLengthOfWords(word1, word2) {
+  var lengtha = word1.length;
+  var lengthb = word2.length;
+  var added = lengtha + lengthb;
+  var final = added / 2;
+  return(final)
+  // your code here
+}
+
+computeAverageLengthOfWords('code', 'programs');
