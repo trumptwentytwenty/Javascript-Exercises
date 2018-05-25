bc81e00a978057dfa9f1f36d26b7dae8e217815f
trumptwentytwenty <39625985+trumptwentytwenty@users.noreply.github.com>
2018-05-25 09:18:20 -0500

    Create CheckAge.js

---
+++ CheckAge.js
@@ -0,0 +1,9 @@
+function checkAge(name, age) {
+  if (age < 21)
+    return("Go home, " + name + "!")
+  if (age >= 21)
+    return("Welcome, " + name + '!')
+  // your code here
+}
+
+checkAge('Adrian', 22);
